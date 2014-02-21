f888359b3274ac3940aee5959c3d3b9fc6bc749d
jvberkel <jvberkel@users.noreply.github.com>
2014-02-21 21:55:28 +0100

    Renamed Classy to PrintHello, added print of "Hello" to system out

--- TESTAPP/src/PrintHello.java
+++ TESTAPP/src/PrintHello.java
@@ -1,10 +1,9 @@
-
-public class Classy {
+import java.lang.System;
+public class PrintHello {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		// blaat
-
+		System.out.println("Hello");
 	}
-
 }
