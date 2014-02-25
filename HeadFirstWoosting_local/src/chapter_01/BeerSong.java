75bcfd4186bf2bfc8826e9f82671b6eb99271e3e
woosting <github@woosting.nl>
2014-02-25 09:11:41 +0100

    Personal exercises 'Head First Java'

---
+++ HeadFirstWoosting_local/src/chapter_01/BeerSong.java
@@ -0,0 +1,28 @@
+package chapter_01;
+public class BeerSong {
+	public static void main(String[] args) {
+		int beerNum = 3;
+		String word = "bottles";
+		while (beerNum > 0) {
+			if (beerNum == 1) {
+				word = "bottle"; // singular, as in ONE bottle.
+			}
+			System.out.println(beerNum + " " + word + " of beer on the wall");
+			System.out.println(beerNum + " " + word + " of beer.");
+			System.out.println("Take one down.");
+			System.out.println("Pass it around.");
+			beerNum = beerNum - 1;
+			if (beerNum > 0) {
+				
+				if (beerNum == 1) {										// Plural fix - START
+					word = "bottle"; // singular, as in ONE bottle.		// Plural fix
+				} 														// Plural fix - END
+				
+				System.out.println(beerNum + " " + word
+						+ " of beer on the wall");
+			} else {
+				System.out.println("No more bottles of beer on the wall");
+			} // end else
+		} // end while loop
+	} // end main method
+} // end class
---
+++ HeadFirstWoosting_local/src/chapter_01/MyFirstApp.java
@@ -0,0 +1,7 @@
+package chapter_01;
+
+public class MyFirstApp {
+	public static void main (String[] args) {
+		System.out.println("I Rule!");
+		System.out.println("The World");
+		}}
