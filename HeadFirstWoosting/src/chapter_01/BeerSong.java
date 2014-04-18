69d26872439e829f772ca4bd1997c5a2ced5d19f
Leon van Daal <leon.van.daal@gmail.com>
2014-04-18 17:16:09 +0200

    Added the simplest logic to make this test pass. Remember: no logic without a failing test.

--- HeadFirstWoosting/src/chapter_01/BeerSong.java
+++ HeadFirstWoosting/src/chapter_01/BeerSong.java
@@ -4,6 +4,12 @@ public class BeerSong {
 	}
 
 	public static void singBeerSong(StringBuilder songBuilder, int numberOfBottles) {
+		if (numberOfBottles > 0) {
+			songBuilder.append("1 bottle of beer on the wall\n");
+			songBuilder.append("1 bottle of beer.\n");
+			songBuilder.append("Take one down.\n");
+			songBuilder.append("Pass it around.\n");
+		}
 		songBuilder.append("No more bottles of beer on the wall\n");
 	}
 }
