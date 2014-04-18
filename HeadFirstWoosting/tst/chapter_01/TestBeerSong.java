f1d667c1afd706f7b8ad3517d90385638f37d661
Leon van Daal <leon.van.daal@gmail.com>
2014-04-18 14:42:22 +0200

    Added another test about negative number of bottles that succeeds immediately. Note that I did not skip the Refactor phase, but I decided that no refactoring was needed. Also note that although it is not done to add functionality without adding tests first, it's okay to add test without functionality.

--- HeadFirstWoosting/tst/chapter_01/TestBeerSong.java
+++ HeadFirstWoosting/tst/chapter_01/TestBeerSong.java
@@ -15,4 +15,21 @@ public class TestBeerSong {
 				"No more bottles of beer on the wall\n";
 		assertEquals(expectedSong, songBuilder.toString());
 	}
+
+	/**
+	 * Test that a negative amount of beers also means that the wall is empty.
+	 * This choice was made because this was the easiest implementation.
+	 * Normally I would test that an exception is thrown on these types of
+	 * input, because it probably means that something is wrong (it is not
+	 * logical to request a song about a negative number of bottles).
+	 */
+	@Test
+	public void testWallWithNegativeBeers() {
+		StringBuilder songBuilder = new StringBuilder();
+		BeerSong.singBeerSong(songBuilder, -1);
+		
+		String expectedSong =
+				"No more bottles of beer on the wall\n";
+		assertEquals(expectedSong, songBuilder.toString());
+	}
 }
