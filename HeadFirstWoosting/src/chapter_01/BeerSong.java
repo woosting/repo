d30503ce2a3ebd7677eb4acd2d9f8fd0f48bf485
Leon van Daal <leon.van.daal@gmail.com>
2014-04-18 14:30:09 +0200

    Made an implementation that makes the test succeed. This implementation is very trivial, this is meant to be like this. Only add functionality for which the need is shown by a failing unit test. Now we have done the second step of the TDD cycle (Red, Green, Refactor), our tests pass (are Green).

--- HeadFirstWoosting/src/chapter_01/BeerSong.java
+++ HeadFirstWoosting/src/chapter_01/BeerSong.java
@@ -1,10 +1,9 @@
 package chapter_01;
 public class BeerSong {
 	public static void main(String[] args) {
-
 	}
 
 	public static void singBeerSong(StringBuilder songBuilder, int numberOfBottles) {
-		
+		songBuilder.append("No more bottles of beer on the wall\n");
 	}
 }
