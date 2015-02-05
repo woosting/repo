180619332a45b8c4257a1710a733a32c2b0f24c7
woosting <github@woosting.nl>
2015-02-05 15:37:44 +0100

    Another guthub interaction test.

---
+++ HeadFirstWoosting/src/chapter_02/BeerSong.java
@@ -0,0 +1,34 @@
+package chapter_02;
+public class BeerSong {
+	public static void main(String[] args) {
+		StringBuilder songBuilder = new StringBuilder();
+		singBeerSong(songBuilder, 3);
+		System.out.println(songBuilder);
+	}
+
+	public static void singBeerSong(StringBuilder songBuilder, int numberOfBottles) {
+		int beerNum = numberOfBottles;
+		String word = "bottles";
+		
+		while (beerNum > 0) {
+			if (beerNum == 1) {
+				word = "bottle"; // singular, as in ONE bottle.
+			}
+			songBuilder.append(beerNum + " " + word + " of beer on the wall\n");
+			songBuilder.append(beerNum + " " + word + " of beer.\n");
+			songBuilder.append("Take one down.\n");
+			songBuilder.append("Pass it around.\n");
+			beerNum = beerNum - 1;
+			if (beerNum > 0) {
+				
+				if (beerNum == 1) {										// Plural fix - START
+					word = "bottle"; // singular, as in ONE bottle.		// Plural fix
+				} 														// Plural fix - END
+				
+				songBuilder.append(beerNum + " " + word
+						+ " of beer on the wall\n");
+			}
+		}
+		songBuilder.append("No more bottles of beer on the wall\n");
+	}
+}
