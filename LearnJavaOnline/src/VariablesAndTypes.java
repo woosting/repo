554ac12e78694d9cabe0f02364204797604ed2f2
woosting <github@woosting.nl>
2015-02-06 17:50:09 +0100

    Added new project for beginners

---
+++ LearnJavaOnline/src/VariablesAndTypes.java
@@ -0,0 +1,14 @@
+public class VariablesAndTypes {
+    public static void main(String[] args) {
+        byte zero = 0;
+        short one = 1;
+        int ello = 3110;
+//        long;
+//        float f = 2.0f;
+        double d = 2.0;
+        char c = ' ';
+        boolean b = true;
+        String output = "H" + ello + c + "w" + zero + "r" + one + "d" + c + d + c + b;
+        System.out.println(output);
+    }
+}
