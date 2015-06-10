e76f9801a6f14e1293fea2e2fba034deb1d2ee8c
woosting <git@woosting.nl>
2015-06-10 21:20:50 +0200

    Changed the ammount of beers.

--- HeadFirstWoosting/src/chapter_01/BeerSong.java
+++ HeadFirstWoosting/src/chapter_01/BeerSong.java
@@ -2,7 +2,7 @@ package chapter_01;
 public class BeerSong {
 	public static void main(String[] args) {
 		StringBuilder songBuilder = new StringBuilder();
-		singBeerSong(songBuilder, 3);
+		singBeerSong(songBuilder, 5);
 		System.out.println(songBuilder);
 	}
 
